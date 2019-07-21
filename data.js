cf792c1fc30dd323ce3a44c31a21da14d0799330
Travis Rollins <troll59910@aol.com>
2019-07-21 16:47:20 -0600

    Initial commit

---
+++ data.js
@@ -0,0 +1,80 @@
+module.exports = {
+  users: [
+    {
+      id: 1,
+      name: 'Travis Rollins',
+      status: 'online',
+      interests: 'Music, Software, & Gaming'
+    },
+    {
+      id: 2,
+      name: 'Leta Keane',
+      status: 'online',
+      interests: 'Science, Music, & Classic Films'
+    },
+    {
+      id: 3,
+      name: 'Louisa Barrett',
+      status: 'offline',
+      interests: 'Giphs, DJ Khaled, & Unicorns'
+    },
+    {
+      id: 4,
+      name: 'Robbie Jaeger',
+      status: "online",
+      interests: 'Plants & Woodwork'
+    }
+  ],
+  sportTeams: [
+    {
+      id: 1,
+      name: 'Dallas Cowboys',
+      head_coach: 'Jason Garrett',
+      sport: 'football'
+    },
+    {
+      id: 2,
+      name: 'New York Yankees',
+      head_coach: 'Aaron Boone',
+      sport: 'baseball'
+    },
+    {
+      id: 3,
+      name: 'Los Angeles Lakers',
+      head_coach: 'Frank Vogel',
+      sport: 'basketball'
+    },
+    {
+      id: 4,
+      name: 'Philadelphia Flyers',
+      head_coach: 'Alain Vigneault',
+      sport: 'hockey'
+    }
+  ],
+  animals: [
+    {
+      id: 1,
+      name: 'otters',
+      diet: 'urchins, snails, fish & crabs',
+      fun_fact: 'They have the thickest fur of any mammal in the animal kingdom..'
+    },
+    {
+      id: 2,
+      name: 'grizzly bears',
+      diet: 'seeds, berries, roots, grasses, fungi, deer, elk, fish & insects',
+      fun_fact: 'The grizzly bear is the official animal of the state of Montana since 1982.'
+    },
+    {
+      id: 3,
+      name: 'orcas',
+      diet: 'ea birds, squid, octopuses, sea turtles, sharks, rays & fish',
+      fun_fact: 'Orcas, also known as killer whales, are known to prey on other marine mammals, including dolphins and seals.'
+    },
+    {
+      id: 4,
+      name: 'tigers',
+      diet: 'chital, sambar, gaur & wild board',
+      fun_fact: 'The main food of tigers are buffalos, antelopes, and rodents.'
+    }
+  ]
+}
---
+++ server.js
@@ -0,0 +1,136 @@
+const express = require('express');
+const cors = require('express-cors');
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 3001;
+const app = express();
+const { users, sportTeams, animals } = require('./data.js');
+
+app.locals = {
+  title: 'Network Request Practice',
+  users,
+  sportTeams,
+  animals
+}
+
+app.use(cors({
+  allowedOrigins: ['localhost:3000']
+}));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/api/v1/users', (req, res) => {
+  res.status(200).json(app.locals.users);
+});
+
+app.post('/api/v1/users', (req, res) => {
+  const newUser = req.body;
+
+  for (let requiredParameter of ['id', 'name', 'status', 'interests']) {
+    if (!newUser[requiredParameter]) {
+      return res.status(422).json({
+        message: `You are missing a required parameter of ${requiredParameter}`
+      });
+    }
+  }
+
+  app.locals.users = [...app.locals.users, { ...newUser, status: false }];
+
+  return res.status(201).json({id: newUser.id});
+});
+
+app.delete('/api/v1/users/:id', (req, res) => {
+  const { id } = req.params;
+  const match = app.locals.users.find(user => user.id == id);
+
+  if (!match) {
+    return res.status(404).json({
+      message: `No user found with an id of ${id}`
+    });
+  }
+
+  const filteredUsers = app.locals.users.filter(user => user.id != id);
+
+  app.locals.users = filteredUsers;
+
+  return res.sendStatus(204);
+});
+
+app.get('/api/v1/sport-teams', (req, res) => {
+  res.status(200).json(app.locals.users);
+});
+
+app.post('/api/v1/sport-teams', (req, res) => {
+  const newSportTeam = req.body;
+
+  for (let requiredParameter of ['id', 'name', 'head_coach', 'sport']) {
+    if (!newSportTeam[requiredParameter]) {
+      return res.status(422).json({
+        message: `You are missing a required parameter of ${requiredParameter}`
+      });
+    }
+  }
+
+  app.locals.sportTeams = [...app.locals.sportTeams, { ...newSportTeam, status: false }];
+
+  return res.status(201).json({id: newSportTeam.id});
+});
+
+app.delete('/api/v1/sport-teams/:id', (req, res) => {
+  const { id } = req.params;
+  const match = app.locals.sportTeams.find(sportTeam => sportTeam.id == id);
+
+  if (!match) {
+    return res.status(404).json({
+      message: `No sport team found with an id of ${id}`
+    });
+  }
+
+  const filteredSportTeams = app.locals.sportTeams.filter(user => user.id != id);
+
+  app.locals.sportTeams = filteredSportTeams;
+
+  return res.sendStatus(204);
+});
+
+app.get('/api/v1/animals', (req, res) => {
+  res.status(200).json(app.locals.users);
+});
+
+app.post('/api/v1/animals', (req, res) => {
+  const newAnimal = req.body;
+
+  for (let requiredParameter of ['id', 'name', 'diet', 'fun_fact']) {
+    if (!newAnimal[requiredParameter]) {
+      return res.status(422).json({
+        message: `You are missing a required parameter of ${requiredParameter}`
+      });
+    }
+  }
+
+  app.locals.animals = [...app.locals.animals, { ...newAnimal, status: false }];
+
+  return res.status(201).json({id: newAnimal.id});
+});
+
+app.delete('/api/v1/animals/:id', (req, res) => {
+  const { id } = req.params;
+  const match = app.locals.animals.find(sportTeam => sportTeam.id == id);
+
+  if (!match) {
+    return res.status(404).json({
+      message: `No animal found with an id of ${id}`
+    });
+  }
+
+  const filteredAnimals = app.locals.animals.filter(user => user.id != id);
+
+  app.locals.animals = filteredAnimals;
+
+  return res.sendStatus(204);
+});
+
+app.listen(port, () => {
+  console.log(`${app.locals.title} is now running on ${port}!`)
+});
+
