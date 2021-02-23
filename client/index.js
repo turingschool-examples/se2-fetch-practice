8153eb0c85c61f0a8c29b7dd15f70c609d743a33
David Whitaker <damwhit@gmail.com>
2021-02-23 10:59:41 -0700

    Complete step 2

--- client/index.js
+++ client/index.js
@@ -2,7 +2,33 @@ console.log('so fetch!')
 
 // Pick between 1 of the resources available in the api (users, animals, or sports teams), and do the following:
 // 1. Make a get request to get all of the items for a given resource, and log those items to the console.
+const animalsUrl = 'http://localhost:3001/api/v1/animals';
+const getAllAnimals = () => {
+  fetch(animalsUrl)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+  })
+}
+getAllAnimals();
 // 2. Make a post request to create a new instance of that resource, and log the result to the console.
+const addAnimal = () => {
+  const newAnimal = {
+    id: 55,
+    name: 'david',
+    diet: 'burritos',
+    fun_fact: 'loves ramen as well'
+  };
+  fetch(animalsUrl, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(newAnimal)
+  })
+  .then(response => response.json())
+  .then(json => console.log(json))
+}
+
+addAnimal();
 // 3. Make another get request to get the updated data set, and log the result to the console.
 
 // Hook it up to the dom!
