5060b53076112587a28089cf7b6f07e729818b7d
David Whitaker <damwhit@gmail.com>
2021-02-23 15:05:52 -0700

    Update solution to be a bit more readable

--- client/index.js
+++ client/index.js
@@ -1,51 +1,47 @@
-console.log('so fetch!')
-
-// Pick between 1 of the resources available in the api (users, animals, or sports teams), and do the following:
-const animalsSection = document.querySelector('.js-collection');
 const addAnimalsToPage = animals => {
   animals.forEach(animal => {
     addAnimalToPage(animal);
-  })
+  });
 }
 
 const addAnimalToPage = animal => {
-  animalsSection.innerHTML += `<p>${animal.name}</p>`
+  const animalsSection = document.querySelector('.js-collection');
+  animalsSection.innerHTML += `<p>${animal.name}</p>`;
 }
+
 // 1. Make a get request to get all of the items for a given resource, and log those items to the console.
 const animalsUrl = 'http://localhost:3001/api/v1/animals';
 const getAllAnimals = () => {
   fetch(animalsUrl)
   .then(response => response.json())
-  .then(animals => {
-    addAnimalsToPage(animals)
-  });
+  .then(animals => addAnimalsToPage(animals));
 }
 
-getAllAnimals();
 // 2. Make a post request to create a new instance of that resource, and log the result to the console.
-const addAnimal = () => {
-  const newAnimal = {
-    id: 55,
-    name: 'david',
-    diet: 'burritos',
-    fun_fact: 'loves ramen as well'
-  };
+const addAnimal = (newAnimal) => {
   fetch(animalsUrl, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(newAnimal)
   })
   .then(response => response.json())
-  .then(animal => {
-    addAnimalToPage(animal);
-  });
+  .then(animal => addAnimalToPage(animal));
 }
-
-addAnimal();
 
 // Hook it up to the dom!
 // 4. On page load, fire off #1 from above and populate the section.js-collection with those items
-// 5. Add an event listener, so that when you click button.js-add-item, it will make a fetch request to post that item to the server, AND update the page with the newly updated collection
+getAllAnimals();
+
+// 5. Add an event listener, so that when you click button.js-add-item, it will make a fetch request to post that item to the server
+document.querySelector('.js-add-item').addEventListener('click', () => {
+  const newAnimal = {
+    id: 55,
+    name: 'coleslaw',
+    diet: 'burritos',
+    fun_fact: 'loves ramen as well'
+  };
+  addAnimal(newAnimal);
+});
 
 // EXTRA CREDIT:
 // Create a form and event listener so that the user can add an item of their choosing
