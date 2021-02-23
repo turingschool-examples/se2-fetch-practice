c57b840cd3e76830ceb92b0e561fa525bf5af3b4
David Whitaker <damwhit@gmail.com>
2021-02-23 11:03:43 -0700

    Udpate exercise instructions

--- client/index.js
+++ client/index.js
@@ -3,7 +3,6 @@ console.log('so fetch!')
 // Pick between 1 of the resources available in the api (users, animals, or sports teams), and do the following:
 // 1. Make a get request to get all of the items for a given resource, and log those items to the console.
 // 2. Make a post request to create a new instance of that resource, and log the result to the console.
-// 3. Make another get request to get the updated data set, and log the result to the console.
 
 // Hook it up to the dom!
 // 4. On page load, fire off #1 from above and populate the section.js-collection with those items
@@ -11,4 +10,4 @@ console.log('so fetch!')
 
 // EXTRA CREDIT:
 // Create a form and event listener so that the user can add an item of their choosing
-// Handle the use case where a 422 is given back from the server
+// Handle the use case where a 422 is given back from the server (this will happen if all params aren't sent with the post)
