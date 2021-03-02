04883a32c1e8b75fa998eb1ff2ca921b22be8440
David Whitaker <damwhit@gmail.com>
2021-03-02 11:53:56 -0700

    Add error handling

--- client/index.js
+++ client/index.js
@@ -1,8 +1,16 @@
+const checkForError = response => {
+  if (!response.ok) {
+    throw new Error('Please make sure that all fields are filled out.');
+  } else {
+    return response.json();
+  }
+}
 
 const getAllAnimals = () => {
   fetch(animalsUrl)
   .then(response => response.json())
-  .then(animals => addAnimalsToPage(animals));
+  .then(animals => addAnimalsToPage(animals))
+  .catch(err => displayErrorMesssage(err));
 }
 
 const addAnimal = (newAnimal) => {
@@ -11,14 +19,22 @@ const addAnimal = (newAnimal) => {
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(newAnimal)
   })
-  .then(response => response.json())
-  .then(animal => addAnimalToPage(animal));
+  .then(checkForError)
+  .then(animal => addAnimalToPage(animal))
+  .catch(err => displayErrorMesssage(err));
 }
 
 const animalsSection = document.querySelector('.js-animals');
 const animalsUrl = 'http://localhost:3001/api/v1/animals';
 getAllAnimals();
 
+
+const displayErrorMesssage = (err) => {
+  const errorField = document.querySelector('.js-error');
+  const message = err.message === 'Failed to fetch' ? 
+    'Something went wrong. Please check your internet connection' : err.message;
+  errorField.innerText = message;
+}
 
 const addAnimalsToPage = animals => {
   animals.forEach(animal => {
